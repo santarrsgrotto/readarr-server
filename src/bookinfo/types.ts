27b6abbbf05cb510b6098786e8d65143945f36cb
santarrsgrotto <santarrsgrotto@users.noreply.github.com>
2025-03-30 16:26:38 +0100

    use interfaces

--- src/bookinfo/types.ts
+++ src/bookinfo/types.ts
@@ -1,115 +1,103 @@
-export type BookInfoAuthor = {
-  ForeignId: number
-  Name: string
-  Description: string
-  ImageUrl: string
-  Url: string
-  RatingCount: number
-  AverageRating: number
-  Works?: BookInfoWork[]
-  Series?: BookInfoSeries[] | null
+export interface BookInfoAuthor {
+  ForeignId: number;
+  Name: string;
+  Description: string;
+  ImageUrl: string;
+  Url: string;
+  RatingCount: number;
+  AverageRating: number;
+  Works?: BookInfoWork[];
+  Series?: BookInfoSeries[] | null;
 }
 
-export type BookInfoBook = {
-  ForeignId: number
-  Title: string
-  TitleWithoutSeries?: string
-  Description: string
-  Isbn13: string
-
-  CountryCode: string
-  Language: string | null
-  Format: string
-  EditionInformation: string
-  Publisher: string
-  ReleaseDate: string
-
-  IsEbook: boolean
-  NumPages: number
-  RatingCount: number
-  AverageRating: number
-
-  ImageUrl: string
-  Url: string
-
+export interface BookInfoBook {
+  ForeignId: number;
+  Title: string;
+  TitleWithoutSeries?: string;
+  Description: string;
+  Isbn13: string;
+  CountryCode: string;
+  Language: string | null;
+  Format: string;
+  EditionInformation: string;
+  Publisher: string;
+  ReleaseDate: string;
+  IsEbook: boolean;
+  NumPages: number;
+  RatingCount: number;
+  AverageRating: number;
+  ImageUrl: string;
+  Url: string;
   Contributors: {
-    ForeignId: number
-    Role: string
-  }[]
+    ForeignId: number;
+    Role: string;
+  }[];
 }
 
-export type BookInfoSeries = {
-  ForeignId: number
-  Title: string
-  Description: string
-
-  LinkItems: BookInfoSeriesLinkItem[]
+export interface BookInfoSeries {
+  ForeignId: number;
+  Title: string;
+  Description: string;
+  LinkItems: BookInfoSeriesLinkItem[];
 }
 
-export type BookInfoSeriesLinkItem = {
+export interface BookInfoSeriesLinkItem {
   // TODO: Goodreads flags series as being primary or secondary, but OL has no equivalent
-  Primary: boolean
+  Primary: boolean;
   // Starting at 1
-  PositionInSeries: string
-  SeriesPosition: number
-  ForeignSeriesId: number
-  ForeignWorkId: number
+  PositionInSeries: string;
+  SeriesPosition: number;
+  ForeignSeriesId: number;
+  ForeignWorkId: number;
 }
 
-export type BookInfoWork = {
-  ForeignId: number
-  Title: string
-  OriginalTitle?: string
-  ReleaseDate: string
-  Url: string
-  Genres: string[]
-  RatingCount: number
-  AverageRating: number
-  Authors?: BookInfoAuthor[] | null
+export interface BookInfoWork {
+  ForeignId: number;
+  Title: string;
+  OriginalTitle?: string;
+  ReleaseDate: string;
+  Url: string;
+  Genres: string[];
+  RatingCount: number;
+  AverageRating: number;
+  Authors?: BookInfoAuthor[] | null;
   // Foreign IDs
-  RelatedWorks: number[]
-  Books: BookInfoBook[] | null
-  Series?: BookInfoSeries[]
+  RelatedWorks: number[];
+  Books: BookInfoBook[] | null;
+  Series?: BookInfoSeries[];
 }
 
-export type BookSearch = {
+export interface BookSearch {
   // Unique per query string
-  qid: string
+  qid: string;
 
   // Both strings despite being numbers
-  workId: string
-  bookId: string
-
-  bookUrl: string
-  kcrPreviewUrl: string | null
-
-  title: string
-  bookTitleBare: string
-
+  workId: string;
+  bookId: string;
+  bookUrl: string;
+  kcrPreviewUrl: string | null;
+  title: string;
+  bookTitleBare: string;
   description: {
-    html: string
-    truncated: boolean
-    fullContentUrl: string
-  }
-
-  numPages: number
-  avgRating: string
-  ratingsCount: number
-  imageUrl: string
-
-  author: BookSearchAuthor
-
-  from_search: true
-  from_srp: true
-
+    html: string;
+    truncated: boolean;
+    fullContentUrl: string;
+  };
+  numPages: number;
+  avgRating: string;
+  ratingsCount: number;
+  imageUrl: string;
+  author: BookSearchAuthor;
+  from_search: true;
+  from_srp: true;
   // Index of result in the search results
-  rank: number
+  rank: number;
 }
 
-export type BookSearchAuthor = {
-  id: number
-  name: string
-  isGoodreadsAuthor: boolean
-  profileUrl: string
-  worksListUrl: string
+export interface BookSearchAuthor {
+  id: number;
+  name: string;
+  isGoodreadsAuthor: boolean;
+  profileUrl: string;
+  worksListUrl: string;
 }
--- src/types.ts
+++ src/types.ts
@@ -1,112 +1,107 @@
-export type Id = string
+export type Id = string;
 
-export type Author = Model & {
-  entity_type?: string
-
-  name: string
-  personalName?: string
-  fullerName?: string
-  alternateNames?: string[]
-  title?: string
-
-  bio?: string
-  birthDate?: Date
-  deathDate?: Date
-
-  /** IDs for the cover images */
-  photos?: Id[]
+export interface Model {
+  key: Id;
+  type: string;
+  revision: number;
+  created: Date;
+  lastModified: Date;
 }
 
-export type Edition = Model & {
-  title: string
-  full_title?: string
-  description?: string
-  editionName?: string
+export interface Author extends Model {
+  entity_type?: string;
+  name: string;
+  personalName?: string;
+  fullerName?: string;
+  alternateNames?: string[];
+  title?: string;
 
-  isbn_10: string[]
-  isbn_13: string[]
+  bio?: string;
+  birthDate?: Date;
+  deathDate?: Date;
 
-  series?: { name: string; position?: number }[]
-  works: Id[]
-  workKey: string
-  authors: Id[]
-
-  covers?: Id[]
-
-  publishCountry?: string
-  publishDate?: Date
-  publishers?: string[]
-  contributions?: string[]
-
-  numberOfPages?: number
-  sourceRecords?: string[]
-
-  titleNativeLanguage?: string
-  languages?: string[]
-  translatedFrom?: string[]
-  physicalFormat?: string
+  // IDs for the cover images
+  photos?: Id[];
 }
 
-// Base model
-export type Model = {
-  key: Id
-  type: string
-  revision: number
-  created: Date
-  lastModified: Date
+export interface Edition extends Model {
+  title: string;
+  full_title?: string;
+  description?: string;
+  editionName?: string;
+
+  isbn_10: string[];
+  isbn_13: string[];
+
+  series?: { name: string; position?: number }[];
+  works: Id[];
+  workKey: string;
+  authors: Id[];
+
+  covers?: Id[];
+
+  publishCountry?: string;
+  publishDate?: Date;
+  publishers?: string[];
+  contributions?: string[];
+
+  numberOfPages?: number;
+  sourceRecords?: string[];
+
+  titleNativeLanguage?: string;
+  languages?: string[];
+  translatedFrom?: string[];
+  physicalFormat?: string;
 }
 
-// Aggregate rating for a work
-export type Rating = {
-  workKey: Id
-  editionKey?: Id
-  average: number
-  count: number
+export interface Rating {
+  workKey: Id;
+  editionKey?: Id;
+  average: number;
+  count: number;
 }
 
-// Raw JSON
-export type Record = {
-  key: Id
-  type: { key: string }
-  revision: number
-  last_modified: { type: string; value: string }
-  authors?: { author: { key: string } }[]
-  works?: { key?: string }[]
+export interface Record {
+  key: Id;
+  type: { key: string };
+  revision: number;
+  last_modified: { type: string; value: string };
+  authors?: { author: { key: string } }[];
+  works?: { key?: string }[];
 }
 
-export type Series = {
-  workId: number
-  seriesId: number
-  position: number
-  title: string
+export interface Series {
+  workId: number;
+  seriesId: number;
+  position: number;
+  title: string;
 }
 
-export type Work = Model & {
-  authorKey?: string
+export interface Work extends Model {
+  authorKey?: string;
 
-  title: string
-  subtitle?: string
-  description?: string
+  title: string;
+  subtitle?: string;
+  description?: string;
 
-  covers?: Id[]
-  authors?: Id[]
-
+  covers?: Id[];
+  authors?: Id[];
   // Set by us as cache
-  author?: Author | null
+  author?: Author | null;
 
   // For convenience, this also includes authors
   contributors?: {
-    type: string
-    key: string
-  }[]
+    type: string;
+    key: string;
+  }[];
 
-  /** Related URLs */
-  links?: { url: string; title: string }[]
+  // Related URLs
+  links?: { url: string; title: string }[];
 
-  subjects?: string[]
-  subjectPlaces?: string[]
-  subjectTimes?: string[]
-  subjectPeople?: string[]
+  subjects?: string[];
+  subjectPlaces?: string[];
+  subjectTimes?: string[];
+  subjectPeople?: string[];
 
-  firstPublishDate?: string
+  firstPublishDate?: string;
 }
